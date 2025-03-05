b3ba458b692bef0ed913845a992b9cee6b076228
CEGEPGIM\pdmorel <pdmorel@cegepgim.ca>
2025-03-05 09:29:42 -0500

    Exemple useEffect

--- components/UseEffectWithGPS.js
+++ components/UseEffectWithGPS.js
@@ -1,20 +1,21 @@
-import  { View, Text }  from "react-native"
+import  { View, Text, TextInput }  from "react-native"
 import { useState, useEffect } from 'react'
 import * as Location from 'expo-location'
 
 export default UseEffectWithGPS = () => {
-    const [latitude, setLatitude] = useState()
-    const [longitude, setLongitude] = useState()
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [input, setInput] = useState('Changer!')
 
     useEffect(() => {        
         getCurrentLocation()
-    }, [])
+    }, [input])
 
     const getCurrentLocation = async () => {
         //Requête pour déterminer si l'application possède les droits d'accéder à la localisation de l'appareil
         let {status} = await Location.requestForegroundPermissionsAsync()
         console.log('Permission accordé ? : ' + status)
-        const {coords} = await Location.getCurrentPositionAsync()
+        const {coords} = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.Highest, maximumAge: 10000})
         console.log(coords)
         const {latitude, longitude} =  coords
         setLatitude(latitude)
@@ -27,6 +28,10 @@ export default UseEffectWithGPS = () => {
                        alignItems: 'center', 
                        justifyContent: 'center' }}
         >
+            <TextInput 
+                value={input}
+                onChangeText={setInput}
+            />
             <Text>{latitude}</Text>
             <Text>{longitude}</Text>
         </View>
